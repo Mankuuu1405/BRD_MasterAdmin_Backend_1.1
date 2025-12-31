7d74f17fe7a09901bd3f0ae65e5aefed15328eca
Rutik <bhoylarutik7@gmail.com>
2025-12-31 18:57:48 +0530

    added django-axes in requirements.txt

--- adminpanel/profile_management/views.py
+++ adminpanel/profile_management/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from adminpanel.access_control.permissions import IsMasterAdmin
+# from adminpanel.access_control.permissions import IsMasterAdmin
 from .models import VendorProfile, AgentProfile, ClientProfile
 from .serializers import (
     VendorProfileSerializer,
@@ -13,16 +13,16 @@ from .serializers import (
 class VendorProfileViewSet(ModelViewSet):
     queryset = VendorProfile.objects.all()
     serializer_class = VendorProfileSerializer
-    permission_classes = [IsAuthenticated, IsMasterAdmin]
+    permission_classes = [IsAuthenticated]
 
 
 class AgentProfileViewSet(ModelViewSet):
     queryset = AgentProfile.objects.all()
     serializer_class = AgentProfileSerializer
-    permission_classes = [IsAuthenticated, IsMasterAdmin]
+    permission_classes = [IsAuthenticated]
 
 
 class ClientProfileViewSet(ModelViewSet):
     queryset = ClientProfile.objects.all()
     serializer_class = ClientProfileSerializer
-    permission_classes = [IsAuthenticated, IsMasterAdmin]
+    permission_classes = [IsAuthenticated]
