daad0c874f28b3885ba7fa6bb5e9c1bcfa390188
Prudhvi <dasariprudhvi31761@gmail.com>
2026-01-07 17:02:57 +0530

    product revenue api

--- adminpanel/product_revenue/product_mix_management/serializers.py
+++ adminpanel/product_revenue/product_mix_management/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import ProductMix
+from adminpanel.product_revenue.product_management.models import Product
 
 
 class ProductMixSerializer(serializers.ModelSerializer):
     products = serializers.PrimaryKeyRelatedField(
-        many=True, read_only=False
+        many=True,
+        queryset=Product.objects.all()
     )
 
     class Meta:
@@ -21,3 +23,4 @@ class ProductMixSerializer(serializers.ModelSerializer):
             "is_active",
             "created_at",
         ]
+        read_only_fields = ("created_at",)
--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -14,14 +14,22 @@ router.register(r"roles", GroupViewSet)  # 👈 Groups exposed as Roles
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="admin-login"),
-   
+
+    path("product-revenue/", include("adminpanel.product_revenue.product_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.product_mix_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.charges_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.fees_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.interest_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.penalties_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.repayment_management.urls")),
+    path("product-revenue/", include("adminpanel.product_revenue.moratorium_management.urls")),
+    
     path("access-control/", include("adminpanel.access_control.urls")),
     path("approval-master/", include("adminpanel.approval_master.urls")),
     path("home-dashboard/", include("adminpanel.home_dashboard.urls")),
     path("subscription/", include("adminpanel.subscription_management.urls")),
     path("coupon/", include("adminpanel.coupon_management.urls")),
     path("roles/", include("adminpanel.roles_management.urls")),
-    
     path("rule-management/", include("adminpanel.rule_management.urls")),
     path("agent/", include("adminpanel.agent_management.urls")),
     path("currency-management/", include("adminpanel.currency_management.urls")),
